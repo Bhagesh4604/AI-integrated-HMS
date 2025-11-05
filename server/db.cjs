5de75adff46e817a70212b1d11b30bad7c87a656
Bhagesh <bhageshbiradar820@gmail.com>
2025-11-06 02:01:05 +0530

    Use environment variables for database connection

--- server/db.cjs
+++ server/db.cjs
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'hms',
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
   timezone: 'UTC'
 });
 
