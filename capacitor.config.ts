53b45f29b27aed4cd42c4becf6c61298f68d22b9
Bhagesh <bhageshbiradar820@gmail.com>
2025-11-08 15:03:10 +0530

    again server error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.hms.app',
-  appName: 'Shree Medicare ',
+  appName: 'Shree Medicare HMS',
   webDir: 'dist',
   bundledWebRuntime: false,
 
