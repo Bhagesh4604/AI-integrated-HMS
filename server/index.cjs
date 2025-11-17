cc0088dede03daa81ccbe436845d8cab62a5c811
Bhagesh <bhageshbiradar820@gmail.com>
2025-11-17 15:07:34 +0530

    server

--- server/index.cjs
+++ server/index.cjs
@@ -15,7 +15,7 @@ if (process.env.FCM_SERVER_KEY) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
   });
